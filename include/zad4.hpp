6d4f9b895980fd900b22eb38286986495f1a7725
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-15 09:56:25 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,4 +3,21 @@
 #include <exception>
 #include <vector>
 
+vector <Artysta> stworzZespol (int liczba, string &obiekt)
+{
+	vector <Artysta> artysci;
+	if (obiekt.length() != liczba)
+	{
+		try { throw logic_error("logic error"); }
+		catch (exception& e) { cout << "Rzucono wyjatek"; }
+	}
+	liczba = artysci.size();
+	Artysta artysta_1 = Artysta(obiekt, 1);
+	Artysta artysta_2 = Artysta(obiekt, 2);
+	Artysta artysta_3 = Artysta(obiekt, 3);
+	artysci.push_back(artysta_1);
+	artysci.push_back(artysta_2);
+	artysci.push_back(artysta_3);
+	return artysci;
+}
 // tutaj funkcja stworzZespol
