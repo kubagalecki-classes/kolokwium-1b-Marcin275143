d238b3f5baf51dc582421406adc258e583612493
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-15 09:04:01 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,4 +6,24 @@
 #include <sstream>
 #include <string>
 
+using namespace std;
+
+class Artysta
+{
+public:
+
+	Artysta() {};
+	Artysta(const string& A, const unsigned int& L) : pseudonim(A), liczba_sluchaczy(L) {};
+
+	void graj(const ostream& os) { os << pseudonim << " : " << liczba_sluchaczy; }
+//	virtual void graj(const ostream& os) = 0;
+
+	string get_pseudonim() { return pseudonim; }
+	unsigned int get_liczba_sluchaczy() { return liczba_sluchaczy; }
+
+private:
+
+	string pseudonim = "Anonim";
+	unsigned int liczba_sluchaczy = 0;
+};
 // tutaj klasa Artysta
