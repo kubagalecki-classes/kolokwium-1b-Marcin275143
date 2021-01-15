77b7f15fde69947c3f2ac6aeaceec9b108e8b02f
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-15 09:58:09 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,3 +5,34 @@
 #include <cctype>
 
 // tutaj klasy PopGwiazda, RapGwiazda oraz funkcja stworzArtyste
+class PopGwiazda : public Artysta
+{
+public:
+
+	PopGwiazda() {};
+	PopGwiazda(const string& P, const unsigned int& L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Popgwiazda"; liczba_sluchaczy = 2000; }
+
+	virtual void graj() override { cout << pseudonim << " : " << liczba_sluchaczy; }
+
+private:
+
+	string pseudonim = "Popgwiazda";
+	unsigned int liczba_sluchaczy;
+
+};
+
+class RapGwiazda : public Artysta
+{
+public:
+
+	RapGwiazda() {};
+	RapGwiazda(const string& P, const unsigned int& L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Rapgwiazda"; liczba_sluchaczy = 3000; }
+
+	virtual void graj() override { cout << pseudonim << " : " << liczba_sluchaczy; }
+
+private:
+
+	string pseudonim = "Rapgwiazda";
+	unsigned int liczba_sluchaczy;
+
+};
