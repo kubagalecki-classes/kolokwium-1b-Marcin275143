2e89e975227d44e8a88b23993b567d2f86964214
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-23 16:56:31 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -4,4 +4,16 @@
 #include <deque>
 #include <vector>
 
+class Porownaj { public: bool& operator<(Artysta& a) { if (a.get_liczba_sluchaczy() > 1000) true ? false } } porownanie;
+
+template <typename Artysta, typename Artysta>
+unsigned liczPopularnych(Artysta A1, Artysta A2)
+{
+	vector <Artysta> popularni;
+	A1 = popularni.begin();
+	A2 = popularni.end();
+  unsigned liczba_popularnych = count_if(A1, A2, porownanie());
+	return liczba_popularnych;
+}
+
 // tutaj algorytm liczPopularnych(T begin, T end)
