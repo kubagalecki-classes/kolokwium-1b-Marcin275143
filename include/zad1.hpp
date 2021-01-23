d5fb05ea780d73a5a903c968979301b54d03ff9b
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-23 13:58:42 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -15,15 +15,12 @@ public:
 	Artysta() : pseudonim("Anonim"), liczba_sluchaczy(0) {};
 	Artysta(const string& A, unsigned L) : pseudonim(A), liczba_sluchaczy(L) {};
 
-//	friend ostream& operator << (const ostream& os, const Artysta& A) { os << A.pseudonim << " : " << A.liczba_sluchaczy; return os; }
-
 	void graj(ostream& os) { os << pseudonim << " : " << liczba_sluchaczy; }
-//	virtual void graj() = 0;
 
 	string get_pseudonim() { return pseudonim; }
 	unsigned get_liczba_sluchaczy() { return liczba_sluchaczy; }
 
-	virtual ~Ąrtysta() {};
+	~Ąrtysta() {};
 	
 private:
 
