a06af2e0d3c56be4b6450adce1c3b66943435193
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-23 14:40:16 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -10,15 +10,16 @@ class PopGwiazda : public Artysta
 public:
 
 	PopGwiazda() {};
-	PopGwiazda(const string& P, const unsigned int& L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Popgwiazda"; liczba_sluchaczy = 2000; }
+	PopGwiazda(const string& P, unsigned L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Popgwiazda"; liczba_sluchaczy = 2000; }
 
-	virtual void graj() override { cout << pseudonim << " : " << liczba_sluchaczy; }
+	virtual void graj(ostream &os) override { os << "Popgwiazda: " << pseudonim; }
+
+	~PopGwiazda() {};
 
 private:
 
 	string pseudonim = "Popgwiazda";
-	unsigned int liczba_sluchaczy;
-
+	unsigned liczba_sluchaczy : 1000;
 };
 
 class RapGwiazda : public Artysta
@@ -26,13 +27,23 @@ class RapGwiazda : public Artysta
 public:
 
 	RapGwiazda() {};
-	RapGwiazda(const string& P, const unsigned int& L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Rapgwiazda"; liczba_sluchaczy = 3000; }
+	RapGwiazda(const string& P, unsigned L) : pseudonim(P), liczba_sluchaczy(L) { pseudonim = "Rapgwiazda"; liczba_sluchaczy = 3000; }
 
-	virtual void graj() override { cout << pseudonim << " : " << liczba_sluchaczy; }
+	virtual void graj(ostream &os) override { os << "Rapgwiazda: " << pseudonim; }
+
+	~RapGwiazda() {};
 
 private:
 
 	string pseudonim = "Rapgwiazda";
-	unsigned int liczba_sluchaczy;
-
+	unsigned liczba_sluchaczy : 1500;
 };
+
+Artysta* stworzArtyste(string& obiekt)
+{
+	Artysta* artysta;
+	char a = obiekt.front();
+	if (toupper(obiekt.front()) == a) artysta = &PopGwiazda("BTS", 1234);
+	else artysta = &RapGwiazda("Ye", 4321);
+	return artysta;
+}
