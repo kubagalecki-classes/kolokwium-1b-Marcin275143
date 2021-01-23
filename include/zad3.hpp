165eafd2e7dfa4c1598dafe2850731dcae75a9a9
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-23 16:30:00 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -2,4 +2,19 @@
 
 #include "zad1.hpp"
 
+template <class Instrument>
+class ArtystaPlusInstrument
+{
+public:
+
+	ArtystaPlusInstrument() {};
+	ArtystaPlusInstrument(const Artysta* &A, const Instrument &I) : a(A), i(I) {};
+
+	unsigned koncert() { return(i.liczbaUczestnikow(a.liczba_sluchaczy)); }
+
+private:
+
+	Artysta* a;
+	Instrument i;
+};
 //tutaj szablon ArtystaPlusInstrument
