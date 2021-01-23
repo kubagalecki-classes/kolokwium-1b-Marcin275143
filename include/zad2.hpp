5fd77c127b391517943ebccb38de531a8306fa49
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-23 18:50:45 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -8,20 +8,20 @@
 class PopGwiazda : public Artysta
 {
 public:
-    PopGwiazda(const std::string& s, unsigned f) : Artysta{s, f} {}
-    void graj(std::ostream& o) const override { o << "PopGwiazda: " << getPseudonim(); }
+    PopGwiazda(const string& s, unsigned f) : Artysta{s, f} {}
+    void graj(ostream& o) const override { o << "PopGwiazda: " << getPseudonim(); }
 };
 
 class RapGwiazda : public Artysta
 {
 public:
-    RapGwiazda(const std::string& s, unsigned f) : Artysta{s, f} {}
-    void graj(std::ostream& o) const override { o << "RapGwiazda: " << getPseudonim(); }
+    RapGwiazda(const string& s, unsigned f) : Artysta{s, f} {}
+    void graj(ostream& o) const override { o << "RapGwiazda: " << getPseudonim(); }
 };
 
 Artysta* stworzArtyste(const std::string& s)
 {
-    if (s.front() == std::toupper(s.front()))
+    if (s.front() == toupper(s.front()))
         return new PopGwiazda{"BTS", 1234};
     else
         return new RapGwiazda{"Ye", 4321};
