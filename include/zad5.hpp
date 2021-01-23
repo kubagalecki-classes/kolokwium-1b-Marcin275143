cd6a7393394b1f7ecaacd222c554fdb0c2a5ce4c
Marcin275143 <73649084+Marcin275143@users.noreply.github.com>
2021-01-24 00:21:16 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -6,13 +6,13 @@
 
 //class Porownaj : public Artysta { public: bool& operator<(Artysta& a) { if (a.get_liczba_sluchaczy() > 1000) true ? false; } } porownanie;
 
-template <typename Artysta>
-unsigned liczPopularnych(Artysta A1, Artysta A2)
+template <typename Typ>
+unsigned liczPopularnych(Typ T1, Typ T2)
 {
 //	vector <Artysta> popularni;
 //	A1 = popularni.begin();
 //	A2 = popularni.end();
-  	return count_if(A1, A2, [](const Artysta &A) {return A.getSluchacze() > 1000;});
+  	return count_if(T1, T2, [](const Artysta &A) {return A.getSluchacze() > 1000;});
 }
 
 // tutaj algorytm liczPopularnych(T begin, T end)
